1e5d5d36f1413e8ec857a45f6ffc26fdccaa237e
LEROY482 <60727555+LEROY482@users.noreply.github.com>
2020-02-13 08:04:38 +0300

    Update Divisibility.py

--- Divisibility.py
+++ Divisibility.py
@@ -1,8 +1,8 @@
 #Python program checks whether the given number is divisible by both 2 and 5 using If Else.
 
-number = int(input(" Enter any +ve Integer : ")) #DATA IS BEING INPUTED BY THE USER
+number = int(input(" Enter any +ve Integer : ")) #DATA IS BEING INPUTED BY THE USER #iNT MAKES SURE THE NUMBER IS AN INTERGER.
 
-if((number % 2 == 0) and (number % 5 == 0)):
-    print("Given Number {0} is Divisible by 2 and 5".format(number))
+if((number % 2 == 0) and (number % 5 == 0)): # % STANDS FOR CHECKING FOR THE REMAINDER.
+    print("Given Number {0} is Divisible by 2 and 5".format(number)) # If the remaider is more than 0 it is divisible by 2 or 5
 else:
     print("Given Number {0} is Not Divisible by 2 and 5".format(number))
